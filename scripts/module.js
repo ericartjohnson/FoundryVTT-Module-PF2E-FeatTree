604dab106cb2873930064db1603d2a20bf58d6f6
Eric Johnson <ericartjohnson@gmail.com>
2024-08-22 18:57:02 -0400

    trying a template

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,29 @@
-Hooks.once('init', async function() {
-    console.log("FEATTREE INIT");
+Hooks.on("renderCharacterSheetPF2e", (param1, param2, param3, param4) => {
+    console.log(param1, param2, param3, param4);
+    let feattree = new FeatTreeApplication();
+    feattree.render();
 });
 
-Hooks.once('ready', async function() {
-    console.log("FEATTREE READY");
-});
+class FeatTreeApplication extends Application {
+    constructor(options = {}) {
+        super(options);
+    }
+
+    get title() {
+        return "Feat Tree";
+    }
+
+    static get defaultOptions(){
+        return {
+            ...super.defaultOptions,
+            id: "feattree",
+            classes: [],
+            template: "modules/pf2e-feattree/templates/feattree.hbs",
+            left: 0,
+            top: 0,
+            width: 800,
+            height: 400,
+            resizable: true
+        }
+    }
+}
